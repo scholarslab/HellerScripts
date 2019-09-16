2abc4607d92b108d031916f5d335d90e64b941e7
Shane Lin <ssl2ab@virginia.edu>
2019-09-16 15:35:21 -0400

    output network relationship csv

--- names.py
+++ names.py
@@ -2,6 +2,7 @@ import json
 import csv
 import unidecode
 import re
+import csv
 
 def alphas_only(input):
     words = []
@@ -29,7 +30,7 @@ def gs_name(name):
     else:
         return surname
 
-def gs_name2(name):
+def gs_name_single_initial(name):
     surname = alphas_only(name.split(",")[0])
     fname = name.split(",")[1] if len(name.split(","))>1 else ""
 
@@ -38,52 +39,39 @@ def gs_name2(name):
         return initials[0]+" "+surname
     else:
         return surname
-    
-    
 
 with open('names.csv', mode='r') as csvfile:
     csvreader = csv.reader(csvfile)
     gendermap = {row[0]:row[1] if row[1] else "NULL" for row in csvreader}
-
 gs_gendermap = {gs_name(name):gendermap[name] for name in gendermap}
+gs_gendermap2 = {gs_name_single_initial(name):gendermap[name] for name in gendermap}
 
-gs_gendermap2 = {gs_name2(name):gendermap[name] for name in gendermap}
-
-count = 0
-notfound = 0
+def map_gender(name):
+    if name in gs_gendermap:
+        return gs_gendermap[name]
+    elif name in gs_gendermap2:
+        return gs_gendermap2[name]
+    else:
+        return "UNKNOWN"
 
 with open("JIABS2010-1559678685.json") as datafile:
     data = json.load(datafile)
-    print("Authors:\n")
-    for datum in data.values():
-        for author in datum["docinfo"]["bib"]["author"].split(" and "):
-            count+=1
-            name = gs_name(author)
-            if name in gs_gendermap:
-                print(name, " : ", gs_gendermap[name])
-            elif name in gs_gendermap2:
-                print(name, " : ", gs_gendermap2[name])
-            else:
-                print(name, " : NOT FOUND")
-                notfound+=1
-    print("Total names: ", count)
-    print("Not found names: ", notfound)
-
-    count = 0
-    notfound = 0
-    print("Citation authors:\n")
-    for datum in data.values():
-        for citation in datum["citedby"]:
-            for author in citation["bib"]["author"].split(" and "):
-                count+=1
-                name = gs_name(author)
-                if name in gs_gendermap:
-                    print(name, " : ", gs_gendermap[name])
-                elif name in gs_gendermap2:
-                    print(name, " : ", gs_gendermap2[name])
-                else:
-                    print(name, " : NOT FOUND")
-                    notfound+=1
-    print("Total names: ", count)
-    print("Not found names: ", notfound)
     
+    with open('network.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, dialect='excel')
+        header = ['author', 'author gender','citer']
+        csvwriter.writerow(header)
+
+        for datum in data.values():
+            authors = []
+            for author in datum["docinfo"]["bib"]["author"].split(" and "):
+                authors.append(author)
+            citers = set()
+            for citation in datum["citedby"]:
+                for author in citation["bib"]["author"].split(" and "):
+                    if author not in authors:
+                        citers.add(author)
+            for author in authors:
+                for citer in citers:
+                    csvwriter.writerow([author,map_gender(author),citer])
+                
